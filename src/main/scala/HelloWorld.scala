b314d68326a3bef2243a64b7e6d6385e0d4a6849
Marius Kleiner <kleiner@gmail.com>
2016-09-10 09:18:44 +0200

    Turned app into a conscripted version

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,8 +1,14 @@
 package ch.hotstuff.cstest
 
-object HelloWorld {
-  def main(args: Array[String]) {
-    println("Hello World!!!")
-  }
-}
+class HelloWorld extends xsbti.AppMain {
+  def run(configuration: xsbti.AppConfiguration): xsbti.MainResult = {
+    val scalaVersion = configuration.provider.scalaProvider.version
 
+    // Print a message and the arguments to the application
+    println("Hello world!  Running Scala " + scalaVersion)
+    configuration.arguments.foreach(println)
+
+    new Exit(0)
+  }
+  class Exit(val code: Int) extends xsbti.Exit
+}
